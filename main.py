201281f4981e215bc6d83ce012ecdc45acff333a
dylonputro <deodylon@gmail.com>
2025-06-05 12:29:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-    # Customer Segmentation Dashboard
-    st.subheader("Customer Segmentation")
-    groupByCustomer = prepro.customer_segmentation(groupByCustomer)
-    valueCCount = groupByCustomer["cluster"].value_counts().reset_index()
-    valueCCount.columns = ["cluster", "count"]
-    fig = px.bar(valueCCount, x="cluster", y="count", color="cluster", title="Bar Chart Jumlah Produk per Kategori")
-    st.plotly_chart(fig)
-    # Chatbot Section
-    st.title("🤖 Simple Chatbot with Adashboard")
-    client = ollama.Client()
-    model = "granite3-dense:2b"
-    if "messages" not in st.session_state:
-        st.session_state["messages"] = [{"role": "assistant", "content": "Hi! How can I help you today?"}]
-    for msg in st.session_state.messages:
-        with st.chat_message(msg["role"]):
-            st.markdown(msg["content"])
-    user_input = st.chat_input("Type your message...")
-    if user_input:
-        st.session_state.messages.append({"role": "user", "content": user_input})
-        with st.chat_message("user"):
-            st.markdown(user_input)
-        with st.chat_message("assistant"):
-            with st.spinner("Thinking..."):
-                response = client.generate(model=model, prompt=(user_input))
-                st.markdown(response.response)
-        st.session_state.messages.append({"role": "assistant", "content": response.response})
+
+import pandas as pd 
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import prepro
+import ollama
+import joblib 
+from darts import TimeSeries
+from darts.models import NBEATSModel
+from darts.dataprocessing.transformers import Scaler
+from darts.utils.timeseries_generation import datetime_attribute_timeseries
+from darts.utils.timeseries_generation import holidays_timeseries
+from darts.metrics import mape
+# Set page configuration
+st.set_page_config(layout="wide", page_title="Dashboard Group 15", page_icon="📊")
+# Custom CSS for styling
+st.markdown("""
+    <style>
+    .main {
+        background-color: #f0f2f5;
+    }
+    .title {
+        font-size: 2.5em;
+        color: #1F2A44;
